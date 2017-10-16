7444fdc8d61dafde3c5dcc62c58bc43a62aa6661
Xaioting <ting02280@gmail.com>
2017-10-16 15:07:40 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	float a;
+	a=in.nextFloat();
+	System.out.printf("%.1f",a/600);
+    }
+}
